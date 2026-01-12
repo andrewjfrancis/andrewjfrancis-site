7fb5d4998a124f708f4c8cc5f48a1392e1feb01d
Andrew J. Francis <mail@andrewjfrancis.com>
2026-01-12 17:50:38 -0600

    Update used articles for testing in order to make a commit
    Just added a space to the file for a simple commit.
    
    Signed-off-by: Andrew J. Francis <mail@andrewjfrancis.com>

--- app/writing/_data/articles.ts
+++ app/writing/_data/articles.ts
@@ -403,6 +403,7 @@ export const ARTICLES_TEST: Article[] = [
 
 // Choose active dataset (flip this line whenever you want)
 export const ARTICLES: Article[] = ARTICLES_TEST;
+
 //export const ARTICLES: Article[] = ARTICLES_REAL;
 
 // ---------- helpers (deterministic, boring, correct)
